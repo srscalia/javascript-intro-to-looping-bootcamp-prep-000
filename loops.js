cd0299bf485039d6151d9976fab46c30b1aaa06c
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-26 21:54:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,8 +16,11 @@ function whileLoop(n) {
   return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
 function doWhileLoop(array) {
-  function maybeTrue() {
-  return Math.random() >= 0.5
-}
-}
+  do { 
+  } while ();
+}  
